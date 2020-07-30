2a434587eaae54d5fddc82d60cbed33a321d798b
tomoya yanagibashi <t.bashiyy@gmail.com>
2020-07-30 15:14:56 +0900

    issueで子課題を設定し始めたとき用の修正

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import * as core from '@actions/core';
 async function main(): Promise<void> {
   const repository = core.getInput('repository');
   const branchName = core.getInput('branchName');
+
+  if (branchName.match(/YD-[0-9]+-#[0-9]+/i) !== null) return;
+  const backlogNo = branchName.match(/YD-[0-9]+/i)?.toString().toUpperCase();
+
   const prRef = core.getInput('pr');
   const apiKey = core.getInput('backlogApiKey');
   const prUrl = `https://github.com/${repository}/pull/${prRef.match(/[0-9]+/)?.toString()}`;
 
-  const backlogNo = branchName.match(/YD-[0-9]+/i)?.toString().toUpperCase();
   const branchAttrId = core.getInput('backlogBranchAttributeId');
   const prUrlAttrId = core.getInput('backlogPrUrlAttributeId');
   const backlogEndpoint = core.getInput('backlogEndpoint');
